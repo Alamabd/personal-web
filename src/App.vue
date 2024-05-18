f3e21fae7409ed7519d721ec1462423e4923737f
Alamabd <alamabdillah0108@gmail.com>
2024-05-18 11:47:44 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Nav from './components/Nav.vue';
+</script>
+
+<template>
+  <div class="mx-12">
+    <Header>
+      <Nav />
+    </Header>
+    <main>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,23 @@
+
+<template>
+    <nav class="bg-white flex justify-between">
+        <a href=".">Logo</a>
+        <ul class="flex gap-4">
+            <li>
+                <a href="">My</a>
+            </li>
+            <li>
+                <a href="">teck stack</a>
+            </li>
+            <li>
+                <a href="">project</a>
+            </li>
+            <li>
+                <a href="">contact</a>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<style scoped>
+</style>
