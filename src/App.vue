4390dc2f746f2e193ce48909abf0a86cc2430a9b
Alamabd <alamabdillah0108@gmail.com>
2024-05-18 20:08:12 +0700

    feat: navbar and hero section

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
+import HeroSection from './components/HeroSection.vue';
 import Nav from './components/Nav.vue';
 </script>
 
 <template>
-  <div class="mx-12">
+  <div class="mx-12 mt-4 max-sm:mx-4">
     <Header>
       <Nav />
     </Header>
     <main>
+      <HeroSection />
     </main>
   </div>
 </template>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,21 +1,43 @@
 
 <template>
-    <nav class="bg-white flex justify-between">
-        <a href=".">Logo</a>
-        <ul class="flex gap-4">
-            <li>
-                <a href="">My</a>
-            </li>
-            <li>
-                <a href="">teck stack</a>
-            </li>
-            <li>
-                <a href="">project</a>
-            </li>
-            <li>
-                <a href="">contact</a>
-            </li>
-        </ul>
+    <nav class="navbar bg-base-100 dark:bg-black">
+        <div class="flex-1">
+            <a href="." class="text-xl font-semibold">Alamabd</a>
+        </div>
+        <div class="flex-none">
+            <ul class="menu menu-horizontal px-1">
+                <li>
+                    <a href="#">about</a>
+                </li>
+                <li class="relative">
+                    <details>
+                        <summary>
+                            more
+                        </summary>
+                        <ul class="absolute right-0 p-2 bg-base-100 rounded-t-none">
+                            <li>
+                                <a>Project</a>
+                            </li>
+                            <li>
+                                <a class="text-nowrap">tech stack</a>
+                            </li>
+                            <li>
+                                <label class="flex cursor-pointer gap-2 sm:hidden">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>
+                                    <input type="checkbox" value="synthwave" class="toggle theme-controller"/>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+                                </label>
+                            </li>
+                        </ul>
+                    </details>
+                </li>
+            </ul>
+            <label class="ml-8 flex cursor-pointer gap-2 max-sm:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>
+                <input type="checkbox" value="synthwave" class="toggle theme-controller"/>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+            </label>
+        </div>
     </nav>
 </template>
 
